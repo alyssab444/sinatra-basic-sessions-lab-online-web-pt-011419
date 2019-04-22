ef9c3e1baef88355ab581c1a1b1071203e9f520c
Alyssa Brown <alyssa.brown@live.com>
2019-04-22 22:12:13 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -11,7 +11,8 @@ class App < Sinatra::Base
   end 
   
   post '/checkout' do 
-    @sessions = sessions 
-    params[:items]
+  session[:item] = params[:item]
+    @session = session
+    erb :checkout
   end 
 end
